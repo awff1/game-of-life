b7c5e3555c992e857308a666eff74c033efffce8
Airat <airat.valiullin@tum.de>
2024-06-15 11:51:53 +0200

    add template for get grids

--- source.py
+++ source.py
@@ -1,5 +1,13 @@
-Grid = list[list[bool]]
+Grid = list[list[int]]
+
+
+def get_empty_grid(width: int, height: int) -> Grid:
+    ...
+
+
+def get_random_grid(width: int, height: int) -> Grid:
+    ...
 
 
 def step(grid: Grid) -> Grid:
-    pass
+    ...
