161cdb579e36cef5f05898c90103be4085ba93a6
Azat <azatkep118@gmail.com>
2024-06-19 15:34:17 +0300

    fix issue

--- source.py
+++ source.py
@@ -14,25 +14,22 @@ def get_random_grid(width: int, height: int) -> Grid:
  
 def step(grid: Grid) -> Grid:
     DELTAS =[(-1,1),(0,1),(1,1),(1,0),(1,-1),(0,-1),(-1,-1),(-1,0)]
-    life_cell = 0
     H = len(grid)
     W = len(grid[0])
     new_grid = deepcopy(grid)
     for i in range(H):
         for j in range(W):
-            for dj, di in DELTAS:
-                life_cell = 0
-                new_cell = (i + di, j + dj)
+            life_cell = 0
+            for di, dj in DELTAS:
+                new_cell =  grid[i+di][j+dj]
                 if new_cell == 1:
                     life_cell+=1
             if grid[i][j]:
                 if 1 < life_cell < 4: 
                     new_grid[i][j] = 1
-                    return new_grid
             else:
                 if life_cell == 3: 
                     new_grid[i][j] = 1
-                    return new_grid
                 else:
                     new_grid[i][j] = 0
-                    return new_grid
+    return new_grid
